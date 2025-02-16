8b502d330a6346f1e4a2b13db870030749bceb92
OPNA2608 <opna2608@protonmail.com>
2025-02-16 18:37:37 +0100

    pmdmini: 0-unstable-2023-05-26 -> 2.0-unstable-2024-09-08

--- cfg/pmdmini.nix
+++ cfg/pmdmini.nix
@@ -1,20 +1,28 @@
-{ stdenv, lib, fetchFromGitHub, fetchpatch
-, cmake, pkg-config
-, SDL2
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  fetchpatch,
+  cmake,
+  pkg-config,
+  SDL2,
 }:
 
 stdenv.mkDerivation rec {
   pname = "pmdmini";
-  version = "0-unstable-2023-05-26";
+  version = "2.0-unstable-2024-09-08";
 
   src = fetchFromGitHub {
     owner = "gzaffin";
     repo = "pmdmini";
-    rev = "b878a46ef18b1a8061405b3ac57f5f779f198042";
-    hash = "sha256-CtyobNCNWWGxXeCm9m64Mr9yHcNe4Of7vA9IhcEnVU8=";
+    rev = "88406c57c124ed7034617e78f3700416df552d6f";
+    hash = "sha256-VTJH9RrxJDrTs1HT/33a8jQXjwiNBFmSgpV4qJm5oAQ=";
   };
 
-  nativeBuildInputs = [ cmake pkg-config ];
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+  ];
 
   buildInputs = [ SDL2 ];
 
@@ -28,4 +36,13 @@ stdenv.mkDerivation rec {
   #   "-DCMAKE_BUILD_TYPE=Debug"
   # ];
   # dontStrip = true;
+
+  meta = {
+    description = "Professional Music Driver (PMD) format player using Simple DirectMedia Layer (SDL) version 2.x";
+    homepage = "https://github.com/gzaffin/pmdmini";
+    license = lib.licenses.gpl2Only;
+    mainProgram = "pmdplay";
+    maintainers = [ ]; # lib.maintainers.OPNA, but this isn't Nixpkgs lol
+    platforms = lib.platforms.unix;
+  };
 }
